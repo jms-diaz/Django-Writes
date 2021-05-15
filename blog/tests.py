9ac313f7d95fb2ed199129e9437d8aa3d1d147b7
James Diaz <jamisyes@gmail.com>
2021-05-15 21:44:07 +0800

    added crud functionality

--- blog/tests.py
+++ blog/tests.py
@@ -19,6 +19,9 @@ class BlogTest(TestCase):
             body='Nice body',
             author=self.user
         )
+    
+    def test_get_absolute_url(self):
+        self.assertEqual(self.post.get_absolute_url(), '/post/1')
 
     def test_string_representation(self):
         post=Post(title='sample title')
@@ -42,3 +45,24 @@ class BlogTest(TestCase):
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'A good title')
         self.assertTemplateUsed(response, 'post_detail.html')
+
+    def test_post_create_view(self):
+        response = self.client.post(reverse('post_new'), {
+            'title': 'Test Title',
+            'author': self.user.id,
+            'body': 'Test Body',
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, 'Test Title')
+        self.assertEqual(Post.objects.last().body, 'Test Body')
+
+    def test_post_update_view(self):
+        response = self.client.post(reverse('post_edit', args='1'), {
+            'title': 'Updated Title',
+            'body': 'Updated Body',
+        })
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_delete_view(self):
+        response = self.client.post(reverse('post_delete', args='1'))
+        self.assertEqual(response.status_code, 302)
